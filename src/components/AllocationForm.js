97e81472e613b7aeac57382578bed05e55438bab
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-04-28 18:36:25 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,41 +1,69 @@
-    import React from 'react';
-    import 'bootstrap/dist/css/bootstrap.min.css';
-    import { AppProvider } from './context/AppContext';
-    import Budget from './components/Budget';
-    import ExpenseTotal from './components/ExpenseTotal';
-    import ExpenseList from './components/ExpenseList';
-    import AllocationForm from './components/AllocationForm';
-    import RemainingBudget from './components/Remaining';
-    const App = () => {
+    import React, { useContext, useState } from 'react';
+    import { AppContext } from '../context/AppContext';
+    const AllocationForm = (props) => {
+        const { dispatch,remaining  } = useContext(AppContext);
+        const [name, setName] = useState('');
+        const [cost, setCost] = useState('');
+        const [action, setAction] = useState('');
+        const submitEvent = () => {
+                if(cost > remaining) {
+                    alert("The value cannot exceed remaining funds  £"+remaining);
+                    setCost("");
+                    return;
+                }
+            const expense = {
+                name: name,
+                cost: parseInt(cost),
+            };
+            if(action === "Reduce") {
+                dispatch({
+                    type: 'RED_EXPENSE',
+                    payload: expense,
+                });
+            } else {
+                    dispatch({
+                        type: 'ADD_EXPENSE',
+                        payload: expense,
+                    });
+                }
+        };
         return (
-            <AppProvider>
-                <div className='container'>
-                    <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>
-                        <div className='col-sm'>
-                            <RemainingBudget />
-                        </div>
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>
+            <div>
+                <div className='row'>
+                <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                        <div className="input-group-prepend">
+                    <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+                      </div>
+                      <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                            <option defaultValue>Choose...</option>
+                            <option value="Marketing" name="marketing"> Marketing</option>
+                    <option value="Sales" name="sales">Sales</option>
+                    <option value="Finance" name="finance">Finance</option>
+                    <option value="HR" name="hr">HR</option>
+                    <option value="IT" name="it">IT</option>
+                    <option value="Admin" name="admin">Admin</option>
+                      </select>
+                        <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                    <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                      </div>
+                      <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                            <option defaultValue value="Add" name="Add">Add</option>
+                    <option value="Reduce" name="Reduce">Reduce</option>
+                      </select>
+                        <input
+                            required='required'
+                            type='number'
+                            id='cost'
+                            value={cost}
+                            style={{ marginLeft: '2rem' , size: 10}}
+                            onChange={(event) => setCost(event.target.value)}>
+                            </input>
+                        <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                            Save
+                        </button>
                     </div>
-                    <h3 className='mt-3'>Allocation</h3>
-                    <div className='row '>
-                        <div className='col-sm'>
-                            <ExpenseList />
-                        </div>
                     </div>
-                    <h3 className='mt-3'>Change allocation</h3>
-                    <div className='row mt-3'>
-                        <div className='col-sm'>
-                            <AllocationForm/>
-                        </div>
-                    </div>
-                </div>
-            </AppProvider>
+            </div>
         );
     };
-    export default App;
+    export default AllocationForm;
