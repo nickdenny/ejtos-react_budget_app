308dc26e25bc9b4d03d9894ed920fedcff7a2447
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-05-01 17:40:34 -0700

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -5,6 +5,7 @@
         const [name, setName] = useState('');
         const [cost, setCost] = useState('');
         const [action, setAction] = useState('');
+        const { currency } = useContext(AppContext);
         const submitEvent = () => {
                 if(cost > remaining) {
                     alert("The value cannot exceed remaining funds  £"+remaining);
@@ -50,12 +51,13 @@
                             <option defaultValue value="Add" name="Add">Add</option>
                     <option value="Reduce" name="Reduce">Reduce</option>
                       </select>
+                      <label className="input-group-text" htmlFor="cost">{currency}</label>
                         <input
                             required='required'
                             type='number'
                             id='cost'
                             value={cost}
-                            style={{ marginLeft: '2rem' , size: 10}}
+                            style={{ size: 10}}
                             onChange={(event) => setCost(event.target.value)}>
                             </input>
                         <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
