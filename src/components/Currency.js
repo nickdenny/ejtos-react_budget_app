538163b56738455d7c9b6bcbd75f8aac1de69841
nickdenny <123418391+nickdenny@users.noreply.github.com>
2024-05-01 17:41:21 -0700

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, setState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
     const { currency } = useContext(AppContext);
-    
+    const [newCurrency, setNewCurrency] = useState(currency);
+    const handleCurrencyChange = (option) => {
+        alert(option);
+        setNewCurrency(option.value);
+        setState({currency});
+    }
     return (
         <div>
             <label htmlFor="inputGroupSelect02">currency</label>
-            <select  className="alert alert-success" id="inputGroupSelect02">
+            <select value={currency} className="alert alert-success" id="inputGroupSelect02" onChange={handleCurrencyChange}>
                 <option defaultValue>Choose...</option>
                 <option value="Dollar" name="dollar"> $ Dollar</option>
                 <option value="Pound" name="pound">£ Pound</option>
